36eeb089aa9633acde677cb4a26744b50c12bf4c
thirloksakthivel <sthirlok@gmail.com>
2022-11-24 11:26:19 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:bigbasket/view/loginScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:iconsax/iconsax.dart';
+
+void main() {
+  runApp(const GetMaterialApp(
+    home: loginScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
